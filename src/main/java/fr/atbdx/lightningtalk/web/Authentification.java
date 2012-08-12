b79566c47f036af665f1d1752f5c1dfb056c9f89
Roux Jérôme <roux.jerome@gmail.com>
2012-08-12 21:09:14 +0200

    Récupération simple l'utilisateur google

--- src/main/java/fr/atbdx/lightningtalk/web/Authentification.java
+++ src/main/java/fr/atbdx/lightningtalk/web/Authentification.java
@@ -1,5 +1,6 @@
 package fr.atbdx.lightningtalk.web;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.apache.commons.lang3.StringUtils;
@@ -9,28 +10,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleBrowserClientRequestUrl;
+import com.google.api.client.auth.oauth2.TokenResponseException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.plus.Plus;
+import com.google.api.services.plus.model.Person;
 
 @Controller
 @RequestMapping("/authentification")
 public class Authentification {
-    
+
+    private static final String CLIENT_SECRET = "hVorhxkqTrUt8Qj4Gkctl4yi";
+    private static final String URL_DE_REDIRECTION = "http://localhost:8080/LightningTalk/authentification/google/retour";
+    private static final String CLIENT_ID = "1073813811256.apps.googleusercontent.com";
+
     @RequestMapping(value = "/google", method = RequestMethod.GET)
     public String demanderAuthentificationGoogle(@RequestParam(required = false, value = "error") String codeErreur, @RequestParam(required = false, value = "code") String code) {
-        GoogleBrowserClientRequestUrl urlDeRedirectionGoogle = new GoogleBrowserClientRequestUrl("1073813811256.apps.googleusercontent.com",
-                        "http://localhost:8080/LightningTalk/authentification/google/retour", Arrays.asList("https://www.googleapis.com/auth/userinfo.email",
-                                        "https://www.googleapis.com/auth/userinfo.profile")).setResponseTypes("code").setApprovalPrompt("auto");
-        urlDeRedirectionGoogle.set("access_type", "offline");
-        return "redirect:" + urlDeRedirectionGoogle.build();
+        GoogleAuthorizationCodeRequestUrl demandeDeCodeGoogle = new GoogleAuthorizationCodeRequestUrl(CLIENT_ID,
+                URL_DE_REDIRECTION, Arrays.asList("https://www.googleapis.com/auth/plus.me"));
+        return "redirect:" + demandeDeCodeGoogle.build();
     }
-    
+
     @RequestMapping(value = "/google/retour", method = RequestMethod.GET)
     public @ResponseBody
-    String resultatAuthentificationGoogle(@RequestParam(required = false, value = "error") String codeErreur, @RequestParam(required = false, value = "code") String code) {
+    String resultatAuthentificationGoogle(@RequestParam(required = false, value = "error") String codeErreur, @RequestParam(required = false, value = "code") String code) throws IOException {
         if (StringUtils.isNotEmpty(codeErreur)) {
             return codeErreur;
         } else {
-            return code;
+            GoogleAuthorizationCodeTokenRequest requeteDAuthorisationGoogle = new GoogleAuthorizationCodeTokenRequest(new NetHttpTransport(), new JacksonFactory(),CLIENT_ID,CLIENT_SECRET,code,URL_DE_REDIRECTION);
+            String displayName = "non trouvé";
+            try {
+                GoogleTokenResponse reponse = requeteDAuthorisationGoogle.execute();
+                GoogleCredential googleCredential = new GoogleCredential().setFromTokenResponse(reponse);
+                Plus googlePlus = new Plus.Builder(new NetHttpTransport(),new JacksonFactory(),googleCredential).build();
+                Person profile = googlePlus.people().get("me").execute();
+                displayName = profile.getDisplayName();
+
+                
+            } catch (TokenResponseException e) {
+                e.printStackTrace();
+            }
+            
+            return displayName;
         }
     }
 }
